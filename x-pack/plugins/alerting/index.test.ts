cbff3ead3f836fc4c63a6bfe95c38b8e28897439
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2018-11-06 20:13:25 -0500

    Adds alerting module with failing test.

--- x-pack/plugins/alerting/index.test.ts
+++ x-pack/plugins/alerting/index.test.ts
@@ -3,9 +3,21 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import sinon from 'sinon';
 
-describe('alerting-service', async () => {
+describe('alerting service', async () => {
+  const { alerting } = require('./');
   test('should create a legacy plugin for use', async () => {
-    fail('We cannot stress enough how necessary tests are');
+    const myKibana = { Plugin: () => ({}) };
+    const mock = sinon.mock(myKibana);
+
+    mock
+      .expects('Plugin')
+      .once()
+      .throws();
+    expect(typeof alerting).toEqual('function');
+    const alertingInstance = alerting(myKibana);
+
+    mock.verify();
   });
 });
